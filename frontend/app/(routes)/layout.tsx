13bd160283bb68fe3ae81c4d4609262adde22ba0
boning <wang.boning@outlook.com>
2024-09-09 22:53:16 +1000

    feature: add nextui package to frontend

--- frontend/app/(routes)/layout.tsx
+++ frontend/app/(routes)/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/_components/global.css";
+import { Provider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Food App",
-    description: "A food app.",
+  title: "Food App",
+  description: "A food app.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
 }
