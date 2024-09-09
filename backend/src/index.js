cb9eb0eb07762148536641b2e226054bbbfb9146
xiaobao26 <xiaobao.xue@outlook.com>
2024-09-09 18:47:04 +1000

    chore: update package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,16 +4,21 @@ const app = express();
 const cors = require("cors");
 const helmet = require("helmet");
 
+const morgan = require('./utils/morgan');
+const createLogger = require("./utils/logger");
+const logger = createLogger(__filename);
+
 
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(morgan);
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello world!"});
 })
 
 app.listen(config.PORT, ()=> {
-    console.log(`Server running at ${config.PORT} port`);
+    logger.info(`Server running at ${config.PORT} port`);
 })
