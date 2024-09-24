c4c1c6577ba9038452e1a3ff418b758605b65225
xiaobao26 <xiaobao.xue@outlook.com>
2024-09-24 13:21:17 +1000

    feat: add new function: check users's session and redirect them to login page

---
+++ frontend/middleware.ts
@@ -0,0 +1,9 @@
+export { default } from 'next-auth/middleware';
+
+
+export const config = {
+    //  *: zero or more
+    //  +: one or more
+    //  ?: zero or one
+    matcher: ['/dashboard/:path*']
+}
